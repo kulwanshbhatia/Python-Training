9d8221e74f2d0b0c25d49ce96a6c5291f4938404
kulwanshbhatia <kssinghbhatia@gmail.com>
2025-05-27 12:45:15 +0530

    Search User

--- Day2/crudapp.py
+++ Day2/crudapp.py
@@ -1,6 +1,7 @@
 #import module
-from fastapi import FastAPI
+from fastapi import FastAPI,status,HTTPException
 from pydantic import BaseModel
+from random import randrange
 
 #creating object
 app=FastAPI()
@@ -13,6 +14,18 @@ class User(BaseModel):
     email:str
     city:str
 
+def searchUser(id):
+    for index,data in enumerate(user_data):
+        if data['id']== id:
+            return data
+        
+@app.get('/loaduser/{id}')
+def loadUser(id:int):
+    post=searchUser(id)
+    if post == None:
+       raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="User with Given ID , NOT Found")
+    return {'userdata': post}
+
 @app.get('/loadusers')
 def loadUsers():
     return {'data':user_data}
@@ -20,4 +33,6 @@ def loadUsers():
 @app.post('/adduser')
 def addUser(user:User):
     data=user.model_dump()  #model dump converts input data in key value pairs
-    print(data)
+    data['id']=randrange(0,10000)
+    user_data.append(data)
+    return {'message': data}
